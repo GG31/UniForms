cc9fa656fdeeca310e39150d8c0376c49de6551a
truchi <romain.truchi.06@gmail.com>
2014-11-25 09:11:04 +0100

    include revamp

--- include/connexion.php
+++ include/connexion.php
@@ -1,39 +1,26 @@
-<div id="cont">
-	<div id="connexion">
-		<!-- bloc contenant le formulaire -->
-		<form name="connexionForm" id="connexionForm" action="index.php"
-			method="post">
-			<!-- début du formulaire de connexion -->
-			<fieldset>
-				<legend>Connexion</legend>
-				<!-- titre du formulaire -->
-				<p>
-
-					<span id="erreur"></span>
-					<!-- span qui contiendra les éventuels messages d'erreur -->
-
-				</p>
-				<p>
-					<label for="login">Login :</label> <input type="login" name="login"
-						id="login" />
-					<!-- champ pour le login -->
-				</p>
-
-				<p>
-					<label for="passe">Mot de passe :</label> <input type="password"
-						name="password" id="password" />
-					<!-- champ pour le mot de passe -->
-				</p>
-
-				<p class="center">
-					<input type="submit" value=" Je me connecte" class="bouton" />
-					<!-- bouton de connexion -->
-				</p>
-				<p color="#808080">
-					<a href="#"> > Mot de passe oublié ?</a>
-				</p>
-			</fieldset>
-		</form>
-		<!-- fin du formulaire -->
-	</div>
-</div>
+<?php
+session_start();
+if (! empty ( $_POST )) {
+	// var_dump($_POST);
+	try {
+		// On se connecte à MySQL
+		$bdd = new PDO ( 'mysql:host=localhost;dbname=uniforms', 'root', 'root' );
+	} catch ( Exception $e ) {
+		// En cas d'erreur, on affiche un message et on arrête tout
+		die ( 'Erreur : ' . $e->getMessage () );
+	}
+	$reponse = $bdd->query ( 'SELECT * FROM user' );
+	while ( $donnees = $reponse->fetch () ) {
+		if ($donnees ['login'] == $_POST ['login'] and $donnees ['password'] == md5 ( $_POST ['password'] )) {
+			$_SESSION['user'] = $donnees ['id'];
+			$_SESSION['nom_user'] = $donnees ['nom'];
+			$_SESSION['prenom_user'] = $donnees ['prenom'];
+			// echo "<center><B>Bienvenue Monsieur ".$donnees['nom']."</B></center>";
+			header ( "Location: accueil.php" );
+		} else {
+			echo "<b><center style='color: red;'>Erreur : Login ou Mot de passe</center></B>";
+		}
+	}
+	$reponse->closeCursor ();
+}
+?>  

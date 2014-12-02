a0a4af4d3644a715c19d2ddbc7e302795e4af8fd
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2014-12-02 16:53:16 +0100

    US3 DONE

--- php/include/answers_form.php
+++ php/include/answers_form.php
@@ -1,32 +1,14 @@
 <?php
-//if (isset ( $_GET ["form_id"] ) and isset ( $_GET ["user_id"] )) {
 if (isset ( $_GET ["ans_id"])) {
 	include_once ('include/includes.php');
 	$answer = new Answer($_GET ["ans_id"]);
 	$recipient = $answer->getRecipient();
 	$idform = $answer->getFormId();
-	//echo $idform;
 	$form = new Form($idform);
 	$ans = $form->getListRecipient ( [ ], 1 );
-	
-	/*$form = new Form ( $_GET ["form_id"] );
-	$ans = $form->getListRecipient ( [ ], 1 );*/
-	//var_dump ($ans);
 	$prev = NULL;
 	$next = NULL;
-	//foreach ( $ans as $key => $line ){
-		//var_dump($line["Answer"]);
-		//echo $line["Answer"]->getId();
-		/*if ($line["Answer"]->getId() == $_GET ["ans_id"]){
-			var_dump($line["Answer"]);
-		}*/
-	//}
 	
-	/*foreach ($line["Answer"] as $k => $ln){
-		if ($ln["formDest"] == $_GET ["ans_id"]){
-			echo "oui";
-		}
-	}*/
 	foreach ( $ans as $key => $line ) {
 		if ($line["Answer"]->getId() == $_GET ["ans_id"]) {
 			if ($key + 1 < count ( $ans ))

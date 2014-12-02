0c4a98f23da8b21965f4c37bfa3561a38e97fb6b
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2014-12-02 15:44:46 +0100

    merge verify access

--- php/include/verify_access.php
+++ php/include/verify_access.php
@@ -22,18 +22,20 @@
 				$_SESSION["user_id"] is creator of $_GET["form_id"]
 				$_GET["form_id"] answered by $_GET["user_id"]
 	 */
-	function verify_access_answers(){
+	function verify_access_answers() {
 		$b1 = FALSE;
 		$b2 = TRUE;
-
-		if(isset($_GET["form_id"])){
-			$b1 = (new User($_SESSION["user_id"]))->isCreator($_GET["form_id"]);
-			$b1 ? TRUE : header("Location: error.php?e=1" );
-
-			if(isset($_GET["user_id"])){
-				$ans = (new Form($_GET["form_id"]))->getAnswer([$_GET["user_id"]], 1);
-				$b2  = count($ans) == 1 ? TRUE : FALSE;
-				$b2 ? TRUE : header("Location: error.php?e=2" );
+		
+		if (isset ( $_GET ["form_id"] )) {
+			$b1 = (new User ( $_SESSION ["user_id"] ))->isCreator ( $_GET ["form_id"] );
+			$b1 ? TRUE : header ( "Location: error.php?e=1" );
+			
+			if (isset ( $_GET ["user_id"] )) {
+				$ans = (new Form ( $_GET ["form_id"] ))->getListRecipient ( [ 
+						$_GET ["user_id"] 
+				], 1 );
+				$b2 = count ( $ans ) == 1 ? TRUE : FALSE;
+				$b2 ? TRUE : header ( "Location: error.php?e=2" );
 			}
 		}
 	}

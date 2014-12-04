8ec2891f1d5af3de349b9657465cd8935e641ce3
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2014-12-04 12:43:01 +0100

    Test du Form.class.php et un scenario de test selenium IDE

--- test/Test_FormClass.php
+++ test/Test_FormClass.php
@@ -6,45 +6,77 @@
   require_once('/../php/class/Form.class.php');
   require_once('/../php/class/User.class.php');
   require_once('/../php/class/Answer.class.php');
-
+ 
   class TestOfFormClass extends UnitTestCase {
   	
-    function testConstruct1() {
-	  $Form = new Form(1);
-	  //Test Id
+  	function setUp(){
+  		$Form = new Form(1);
+  	}
+  	
+  	
+  	function tearDown() {
+  		$Form = null;
+    }
+    
+    function testConstruct1_ID() {
+      $Form = new Form(1);
       $this->assertEqual($Form->getId(), 1);
-      
+    }
+    
+    function testConstruct1_CreatorSimple() {
       //Test Creator Simple
+      $Form = new Form(1);
       $Creator = $Form->getCreator();
       $this->assertEqual($Creator->getId(), 1);
       $this->assertEqual($Creator->getName(), 'Luis');
-      
+    }
+
+    function testConstruct1_CreatorObjet() {
       //Teste Creator(object)
+      $Form = new Form(1);
       $user1 = new User(1); // Luis
       $this->assertEqual($Form->getCreator(), $user1);
-      
+    }
+
+    function testConstruct1_State() {
       //Test State
+      $Form = new Form(1);
       $this->assertFalse($Form->getstate());
-      
-      //Test printable
-      $this->assertFalse($Form->getprintable());
-      		
-      //Test anonymous
-      $this->assertFalse($Form->getanonymous());
-      
-      /*
-      //Test Recepient
-      $user2 = new User(2); // Romain
-      $user3 = new User(3); // Genevieve
-      $array = array($user1,  $user1, $user2, $user3);
-      $this->assertIdentical($Form->getRecipient(), $array);
-      */
     }
     
-    function testConstruct2() {
-    	$Form = new Form(-1);
-    	$this->assertFalse($Form->getState());
+    function testConstruct1_Printable() {
+      //Test printable
+      $Form = new Form(1);
+      $this->assertFalse($Form->getprintable());
     }
-  }
 
-  ?>
+    function testConstruct1_Anonymous() {
+      //Test anonymous
+      $Form = new Form(1);
+      $this->assertFalse($Form->getanonymous());
+    } 
+    
+    function testConstruct1_listRecipient(){
+      //Test ListRecepient
+      $Form = new Form(1);
+      $user2 = new User(2); // Romain
+      $user3 = new User(3); // Genevieve
+      $list  = $Form->getListRecipient([], 1);
+      foreach ( $list as $key => $line ){
+      	 $this->assertEqual($line["User"]->getName(), "Luis");
+      }
+    }
+  
+    function testConstruct2_ID() {
+      $Form2 = new Form(-1);
+      $this->assertNull($Form2->getId());
+      
+    }
+    
+    function testConstruct2_State() {
+    	$Form2 = new Form(-1);
+    	$this->assertFalse($Form2->getState());
+    }
+    
+  }
+?>

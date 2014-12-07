9a053bff346c3fee0e03b5836fcb8d0d4ba16135
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2014-12-07 11:49:56 +0100

    Download .csv v0.1 to test

--- php/include/download_csv.php
+++ php/include/download_csv.php
@@ -8,9 +8,32 @@ if (isset ( $_GET ["ans_id"] )) {
 	$form = new Form($idform);
 	$ans = $form->getListRecipient ( [ ], 1 );
 	//var_dump($ans);
+	//echo "<br/><br/><br/><br/>";
+	$outputCsv = '';
+	$outputCsv .= "Exemple Simple d'exportation des donnees en CSV";
+	$outputCsv .= "\n\n\n";
+	$fileName = "Form_" . $_GET["ans_id"] . "Answers";
+	$fileName .= date ( 'Y-m-d_H:i:s' );
+	$fileName .= ".csv";
+	$outputCsv .= "formDest; Destinataire; Status; Answer value";
+	$outputCsv .= "\n";
 	foreach ( $ans as $key => $value ) {
-			echo "formDest : ".$value["Answer"]->getId()."<br/>";
+			//echo " formDest : ".$value["Answer"]->getId().",";
+			$outputCsv .= trim($value["Answer"]->getId()) . ';';
+			//echo " Destinataire : ".$value["User"]->getName().",";
+			$outputCsv .= trim($value["User"]->getName()) . ';';
+			//echo " Status : ".$value["Status"].",";
+			$outputCsv .= trim($value["Status"]) . ';';
+			//var_dump($value["Answer"]->getAnswers());
+			foreach ( $value["Answer"]->getAnswers() as $k => $v ) {
+				//echo " Value : ".$v["value"].",";
+				$outputCsv .= trim($v["value"]) . ';';
+			}
+			$outputCsv = rtrim($outputCsv, ';');
+			$outputCsv .= "\n";
 	}
+	
+	//echo $outputCsv;
 	
 	// la variable qui va contenir les données CSV
 	/*$outputCsv = '';
@@ -31,8 +54,7 @@ if (isset ( $_GET ["ans_id"] )) {
 	 * $outputCsv .= "\n";
 	 * }
 	 */
-	/*$outputCsv .= "Exemple Simple d'exportation des donnees";
-	$outputCsv .= "<br/><br/>";
+	//$outputCsv .= "<br/><br/>";
 	// Entêtes (headers) PHP qui vont bien pour la création d'un fichier Excel CSV
 	header ( "Content-disposition: attachment; filename=" . $fileName );
 	header ( "Content-Type: application/force-download" );
@@ -42,6 +64,6 @@ if (isset ( $_GET ["ans_id"] )) {
 	header ( "Expires: 0" );
 	
 	echo $outputCsv;
-	exit ();*/
+	exit ();
 }
 ?>

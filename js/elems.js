c97060579ac1487c9817f7211b476eb6fa23fc00
truchi <romain.truchi.06@gmail.com>
2014-12-17 16:15:11 +0100

    pfel11 working on it

--- js/elems.js
+++ js/elems.js
@@ -19,19 +19,13 @@ Container = (function(){
 	return Container;
 })();
 
-Unique = (function(){
-	function Unique(obj){
-		unique = '';
-		if(obj.big)
-			unique = this.select(obj);
-		else
-			unique = this.radio(obj);
+Radio = (function(){
+	Radio.prototype.obj;
 
-		return unique;
-	}
+	function Radio(obj){
+		this.obj = obj;
 
-	Unique.prototype.radio = function(obj) {
-		div = $('<div></div>');
+		radio = $('<div></div>');
 
 		length = obj.options.length;
 		for(i = 0; i < length; i++){
@@ -43,18 +37,87 @@ Unique = (function(){
 						.attr('checked', obj.options[i].default == '1' ? true : false)
 						;
 			label.append(input).append(obj.options[i].value);
-			div.append(label);
+			radio.append(label);
 		}
 
-		return div;
-	};
+		return radio;
+	}
 
-	return Unique;
+	Radio.prototype.attrs = function(){
+		// TODO
+		console.log(this);
+	}
+
+	return Radio;
+})();
+
+Select = (function(){
+	Select.prototype.obj;
+
+	function Select(obj){
+		this.obj = obj;
+
+		div = $('<div></div>');
+		select = $('<select></select>');
+
+		length = obj.options.length;
+		for(i = 0; i < length; i++){
+			option = $('<option></option>')
+						.attr('value', 'option-' + obj.options[i].elementoption_id)
+						.attr('selected', obj.options[i].default == '1' ? true : false)
+						.text(obj.options[i].value)
+						;
+			select.append(option);
+		}
+
+		return div.append(select);
+	}
+
+	Select.prototype.attrs = function(){
+		// TODO
+	}
+
+	return Select;
+})();
+
+Multiple = (function(){
+	Multiple.prototype.obj;
+	
+	function Multiple(obj){
+		this.obj = obj;
+
+		multiple = $('<div></div>');
+
+		length = obj.options.length;
+		for(i = 0; i < length; i++){
+			label = $('<label></label>');
+			box = $('<input/>')
+						.attr('type', 'checkbox')
+						.attr('name', 'elem-' + obj.id)
+						.attr('value', 'option-' + obj.options[i].elementoption_id)
+						.attr('checked', obj.options[i].default == '1' ? true : false)
+						;
+			label.append(box).append(obj.options[i].value);
+			multiple.append(label);
+		}
+
+		return multiple;
+	}
+
+	Multiple.prototype.attrs = function(){
+		// TODO
+	}
+
+	return Multiple;
 })();
 
 Element = (function(){
+	Element.prototype.element;
+	Element.prototype.obj;
+
 	function Element(obj, id){
 		console.log('new Element');
+		this.obj = obj;
 
 		element = '';
 		switch(parseInt(obj.type)){
@@ -76,25 +139,68 @@ Element = (function(){
 			case 6:
 				element = $('<textarea></textarea>').attr('height', obj.height + 'px');
 				break;
-			case 7:// TODO pb w/ global attrs
-				element = new Unique(obj);
+			case 7:
+				element = obj.big ? new Select(obj) : new Radio(obj);
+				// console.log(typeof element);
+				(new Radio(obj)).attrs();
 				break;
-			default:
-				element = $('<input/>').attr('type', 'text');
+			case 8:
+				element = new Multiple(obj);
 				break;
 		}
 
-		// TODO 'global' attrs here ! ou pas (textarea)
-		element
-			.attr('id', 'elem-' + obj.id)
-			.attr('placeholder', obj.placeholder)
-			.attr('required', obj.required)
-			.css('width', obj.width + 'px')
-			;
+		this.element = element;
+		this.attrs();
+
 		container = new Container(obj);
 
 		container.append(element).appendTo(id);
 	}
+
+	Element.prototype.attrs = function() {
+		switch(parseInt(this.obj.type)){
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				this.element = this.attrs1();
+			case 7:
+			case 8:
+				//this.element = this.element.attrs();
+		}
+	};
+
+	Element.prototype.attrs1 = function(){// caca TODO rename
+		obj = this.obj;
+		return this.element
+					.attr('id', 'elem-' + obj.id)
+					.attr('placeholder', obj.placeholder)
+					.attr('required', obj.required)
+					.css('width', obj.width + 'px')
+					;
+	}
+
+	Element.prototype.answers = function(answers) {
+		get = typeof answers == 'undefined';
+		res = '';
+
+		switch(parseInt(this.obj.type)){
+			case 1:
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				res = get ? this.getAnswers() : this.setAnswers();
+			case 7:
+			case 8:
+				res = get ? this.element.getAnswers() : this.element.setAnswers();
+		}
+
+		return res;	
+	};
 
 	return Element;
 })();

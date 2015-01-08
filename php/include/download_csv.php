d29bebdd3b881f54efa779f342069ba45084f767
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-01-08 16:44:47 +0100

    PFEL-30 L3 export des résultats CSV

--- php/include/download_csv.php
+++ php/include/download_csv.php
@@ -11,36 +11,43 @@ if (isset ( $_GET ["ans_id"]) AND isset($_GET["user_id"])) {
 	$outputCsv = '';
 	
 	// Entéte de notre fichier
-	$outputCsv .= "Exemple Simple d'exportation des donnees en CSV";
+	//$outputCsv .= "Exemple Simple d'exportation des donnees en CSV";
 	
 	// des espaces pour separer l'entete du contenu
-	$outputCsv .= "\n\n\n";
+	//$outputCsv .= "\n\n\n";
 	
-	// $fileName : c'est le nom du fichier .csv (Form_AnswerID_Answers_date_et_heure_actuel.csv
-	$fileName = "Form_" . $_GET["ans_id"] . "Answers";
+	// $fileName : c'est le nom du fichier .csv (Form_AnswerID_Answers_date_et_heure_actuel.csv)
+	$fileName = $recipient->getName()."";
+	$fileName .= "_Answers_" . $_GET["ans_id"]."_";
 	$fileName .= date ( 'Y-m-d_H:i:s' );
 	$fileName .= ".csv";
 	
 	// Ajouter dans la variable $outputCsv les noms des colonnes
-	$outputCsv .= "No formulaire; Destinataire; Status;";
-	$outputCsv .= "\n";
+	//$outputCsv .= "No formulaire; Destinataire;";
+	//$outputCsv .= "\n";
 	
 	// ici on parcour le resultat de $ans
 	// trim — Supprime les espaces (ou d'autres caractères ex : ";") en début et fin de chaîne 
 	foreach ( $ans as $key => $value ) {
 			if ($value["User"]->getId() == $_GET["user_id"]){
-				$outputCsv .= trim($value["Answer"]->getFormId()) . ';';
-				$outputCsv .= trim($value["User"]->getName()) . ';';
-				$outputCsv .= trim($value["Status"]) . ';';
-				$outputCsv .= "\n";
-				$outputCsv .= "\n";
-				$outputCsv .= "Question; Response";
-				$outputCsv .= "\n";
+				//$outputCsv .= trim($value["Answer"]->getFormId()) . ';';
+				//$outputCsv .= trim($value["User"]->getName()) . ';';
+				//$outputCsv .= trim($value["Status"]) . ';';
+				//$outputCsv .= "\n";
+				//$outputCsv .= "\n";
+				//$outputCsv .= "Question; Response";
+				//$outputCsv .= "\n";
+				//$i = 1;
 				foreach ( $value["Answer"]->getAnswers() as $k => $v ) {
 					$e = new Element($v['elementId']);
 					$outputCsv .= trim($e->getLabel()) . ';';
+					//$outputCsv .= "label" .$i. ';';
+					//$outputCsv .= "\n";
+					//$i++;
+				}
+				$outputCsv .= "\n";
+				foreach ( $value["Answer"]->getAnswers() as $k => $v ) {
 					$outputCsv .= trim($v["value"]) . ';';
-					$outputCsv .= "\n";
 				}
 				$outputCsv = rtrim($outputCsv, ';');
 				$outputCsv .= "\n";

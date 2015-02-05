c8b60ee5d8e8679d08b094adce246dd4b2a1663d
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-02-05 15:05:10 +0100

    PFEL-131 Bug Logout CAS Redirect

--- php/include/logoutCAS.php
+++ php/include/logoutCAS.php
@@ -6,5 +6,8 @@ session_start ();
 	phpCAS::client ( CAS_VERSION_2_0, 'login.unice.fr', 443, '' );
 session_destroy ();
 phpCAS::logout();
+// par defaut il nous rederige vers "https://login.unice.fr/logout"
+//$url="../../index.php";"
+//phpCAS::logoutWithUrl($url);
 header ( "Location: ../../index.php" );
 ?>

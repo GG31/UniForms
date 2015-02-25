fa3a8e04231c965cc3f5c3065dd4984c6295e7af
lfpolo <luisfelipepolo@hotmail.com>
2015-02-25 11:04:23 +0100

    PFEL - 31 Export SQL

---
+++ php/include/exportSQL.php
@@ -0,0 +1,20 @@
+<?php
+include_once ('includes.php');
+if (isset($_GET["form_id"])) {
+	$form = new Form($_GET["form_id"]);
+	if ($form->creator()->id() == $_SESSION["user_id"]){
+		$fileName = "exportedform.sql";
+		/*$file = fopen($fileName, "c");
+		fwrite($file, $form->exportSQL());
+		fclose($file);*/
+		header ( "Content-disposition: attachment; filename=" . $fileName );
+		header ( "Content-Type: application/force-download" );
+		header ( "Pragma: no-cache" );
+		header ( "Cache-Control: must-revalidate, post-check=0, pre-check=0, public" );
+		header ( "Expires: 0" );
+		//readfile($fileName);
+		echo $form->exportSQL();
+		exit ();
+	}
+}
+?>

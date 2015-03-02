e13da55e8689bb31f5edf8efa9df44496bac3f3e
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-03-02 13:02:40 +0100

    Installer.php

--- res/sql/Installer.php
+++ res/sql/Installer.php
@@ -44,6 +44,14 @@
 							name="password" placeholder="password">
 					</div>
 				</div>
+				
+				<div class="form-group">
+					<label for="database" class="col-sm-4 control-label">Database name</label>
+					<div class="col-sm-4">
+						<input type="text" class="form-control" id="dbname"
+							name="dbname" placeholder="uniforms">
+					</div>
+				</div>
 								
 			</div>
 			
@@ -63,16 +71,41 @@
 			$username = $_POST['username'];
 			$password = $_POST['password'];
 			
-			global $db;
-			$db = mysqli_connect ( $server, $username, $password );
-			$sql = "DROP DATABASE IF EXISTS uniforms;";
-			mysqli_query ($database, $sql ) or die ( mysql_error () );
+			$monfichier = fopen('../../php/include/connect.php', 'r+');
+			$contenu = file_get_contents('../../php/include/connect.php');
+				
+			$s1 =  $_POST["server"];
+			$ss1 = "$s1";
+			$contenuMod = str_replace('serverdb', $ss1, $contenu);
+				
+			$s2 =  $_POST["username"];
+			$ss2 = "$s2";
+			$contenuMod = str_replace('userdb', $ss2, $contenuMod );
+				
+			$s3 =  $_POST["password"];
+			$ss3 = "$s3";
+			$contenuMod = str_replace('passworddb', $ss3, $contenuMod);
+				
+			$s4 =  $_POST["dbname"];
+			$ss4 = "$s4";
+			$contenuMod = str_replace('databasedb', $ss4, $contenuMod);
+			//echo htmlentities($contenu);
+			fclose($monfichier);
+				
+			//ouverture en écriture
+			$monfichier2=fopen('../../php/include/connect.php','w+') or die("Fichier manquant");
+			fwrite($monfichier2,$contenuMod);
+			fclose($monfichier2);
 			
-			$sql = "CREATE DATABASE IF NOT EXISTS uniforms;";
-			mysqli_query ($database, $sql ) or die ( mysql_error () );
+			mysql_pconnect ( $server, $username, $password );
+			$sql = "DROP DATABASE IF EXISTS ".$_POST['dbname'].";";
+			mysql_query ( $sql ) or die ( mysql_error () );
 			
-			$sql = "USE uniforms;";
-			mysqli_query ($database, $sql ) or die ( mysql_error () );
+			$sql = "CREATE DATABASE IF NOT EXISTS ".$_POST['dbname'].";";
+			mysql_query ( $sql ) or die ( mysql_error () );
+			
+			$sql = "USE ".$_POST['dbname'].";";
+			mysql_query ( $sql ) or die ( mysql_error () );
 			
 			$fich = fopen ( 'uniforms.sql', "r+" );
 			$lig = "";
@@ -80,7 +113,7 @@
 				$lig .= fgets ( $fich );
 			$req = explode ( ";", $lig );
 			foreach ( $req as $lii ) {
-				mysqli_query ( $lii );
+				mysql_query ( $lii );
 			}
 			fclose ( $fich );
 			

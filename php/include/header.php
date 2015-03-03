30d6c591ee6aa603ce8e13f968827a873060dd8f
truchi <romain.truchi.06@gmail.com>
2015-03-03 22:39:08 +0100

    "papiers" -> "papier" (header)

--- php/include/header.php
+++ php/include/header.php
@@ -1,8 +1,7 @@
 <header class="row page-header">
 	<hgroup>
 		<h1 class="text-center">
-			UniForms<br> <small>Création et partage de formulaires web et papiers
-				!</small>
+			UniForms<br> <small>Création et partage de formulaires web et papier !</small>
 		</h1>
 	</hgroup>
 </header>

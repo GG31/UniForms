48f5ba3c636f5761775b7678c4fa406b20277b51
BENATHMANE <ayoub.benathmane@usmba.ac.ma>
2015-03-04 15:24:19 +0100

    manu utili

--- php/include/footer.php
+++ php/include/footer.php
@@ -1,5 +1,10 @@
 ﻿<footer class="row well">
-	<div class="col-sm-4">UniForms</div>
+	<div class="col-sm-4">UniForms<br/>
+		<a href="../doc/userManual/UserManual.html" style="text-decoration:none">
+			<span class="glyphicon glyphicon-question-sign" aria-hidden="true"></span>
+			Manuel d'utilisation
+		</a>
+	</div>
 	<div class="col-sm-4">
 		BENATHMANE Ayoub, benathmane.ab@gmail.com <br> TRUCHI Romain,
 		romain.truchi.06@gmail.com <br> POLO Luís Felipe,
